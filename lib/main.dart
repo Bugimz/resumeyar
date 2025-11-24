c157aa21ae81841f8ce4fbad66f33d04ceb65272
edris satiyari <101297316+Bugimz@users.noreply.github.com>
2025-11-24 12:54:00 +0330

    Add localization support and language selector

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'app/lang/en_US.dart';
+import 'app/lang/fa_IR.dart';
 import 'app/routes/app_pages.dart';
 import 'app/theme/dark_theme.dart';
 import 'app/theme/light_theme.dart';
 import 'app/theme/theme_controller.dart';
+
+class AppTranslations extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'en_US': enUS,
+        'fa_IR': faIR,
+      };
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,6 +35,9 @@ class MyApp extends StatelessWidget {
     return Obx(
       () => GetMaterialApp(
         title: 'ResumeYar',
+        translations: AppTranslations(),
+        locale: const Locale('en', 'US'),
+        fallbackLocale: const Locale('en', 'US'),
         theme: lightTheme,
         darkTheme: darkTheme,
         themeMode: themeController.themeMode,
